0cda83c496ab8244e5882ba3d96f7588a89e4ef6
Leore Avidar <leore.avidar@gmail.com>
2014-06-22 19:31:38 -0700

    Format Changes

--- test/models/userModel.js
+++ test/models/userModel.js
@@ -1,11 +1,11 @@
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host     : 'localhost',
-    user     : 'lob_user',
-    password : '',
-    database : 'lob_local',
-    charset  : 'utf8'
+    host: 'localhost',
+    user: 'lob_user',
+    password: '',
+    database: 'lob_local',
+    charset: 'utf8'
   }
 });
 
